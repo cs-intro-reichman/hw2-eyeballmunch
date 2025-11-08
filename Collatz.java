b4e5173ed0ca8cf78a3ad719e2adfc78fe1394b3
Adi grayson <adi.grayson30@gmail.com>
2025-11-08 18:54:33 +0200

    first push of the Collatz

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,37 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		
+		int N = Integer.parseInt(args[0]);
+        String mode = args[1];
+
+        for (int seed = 1; seed <= N; seed++) {
+            int current = seed;
+            int steps = 0;
+
+            if (mode.equals("v")) {
+                System.out.print(current);
+            }
+
+            while (current != 1) {
+                steps++;
+                if (current % 2 == 0) {
+                    current /= 2;
+                } else {
+                    current = 3 * current + 1;
+                }
+
+                if (mode.equals("v")) {
+                    System.out.print(" " + current);
+                }
+            }
+
+            if (mode.equals("v")) {
+                steps++; // include the initial number
+                System.out.println(" (" + steps + ")");
+            }
+        }
+
+        System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
 	}
 }

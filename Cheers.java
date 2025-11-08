42824d3aea8844d23ee2b5f5011102be7feddf3d
Adi grayson <adi.grayson30@gmail.com>
2025-11-08 17:56:03 +0200

    first Cheers commit

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,31 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+                //i really have no idea wht the indentaion is 2 tabs its driving me crazzzyyyyy 
+
+                //collecting user input and converting to uppercase
+	        String word = args[0].toUpperCase(); 
+                int times = Integer.parseInt(args[1]);
+
+                //my letters
+                String anLetters = "AEFHILMNORSX";
+
+                for (int i = 0; i < word.length(); i++) {
+                        char ch = word.charAt(i);
+                        String preFix = "a";
+
+                        if (anLetters.indexOf(ch) != -1) {
+                                preFix = "an";
+                        }
+
+                        System.out.println("Give me " + preFix + " " + ch + ": " + ch + "!");
+                }
+
+                // Print "What does that spell?" and repeat
+                System.out.println("What does that spell?");
+
+                for (int i = 0; i < times; i++) {
+                        System.out.println(word + "!!!");
+                }
         }
 }

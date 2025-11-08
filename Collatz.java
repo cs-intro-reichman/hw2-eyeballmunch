5594771f5db649ab7c7168ab84e8380ba7bd73e4
Adi grayson <adi.grayson30@gmail.com>
2025-11-08 19:32:17 +0200

    i kepts having a problem with the printing of the steps

--- Collatz.java
+++ Collatz.java
@@ -15,13 +15,15 @@ public class Collatz {
 
 			// i really tried with all my power not to use do while 
 			do {
+
+				steps++;
+
 				if (current != 1) { 
 					if (current % 2 == 0) {
 						current /= 2;
 					} else {
 						current = 3 * current + 1;
 					}
-					steps++;
 
 					if (mode.equals("v")) {
 						System.out.print(" " + current);

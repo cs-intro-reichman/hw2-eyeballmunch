16d67830db3d62f781222149c7906e48f1518b1c
Adi grayson <adi.grayson30@gmail.com>
2025-11-08 19:13:51 +0200

    mooved steps ++ to after the check

--- Collatz.java
+++ Collatz.java
@@ -14,13 +14,13 @@ public class Collatz {
             }
 
             while (current != 1) {
-                steps++;
+                
                 if (current % 2 == 0) {
                     current /= 2;
                 } else {
                     current = 3 * current + 1;
                 }
-
+				steps ++;
                 if (mode.equals("v")) {
                     System.out.print(" " + current);
                 }

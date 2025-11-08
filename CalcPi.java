5e9858f91bd215b7a3f7aa5652d55cadc5bef865
Adi grayson <adi.grayson30@gmail.com>
2025-11-08 18:34:20 +0200

    first push of pi calculation

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,26 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+
+		// number of terms
+		int nTerms = Integer.parseInt(args[0]); 
+        double sum = 0.0;
+
+        // Compute the series
+        for (int i = 0; i < nTerms; i++) {
+
+            double term = 1.0 / (2 * i + 1); 
+
+            if (i % 2 != 0) { // alternate signs
+                term = -term;
+            }
+            sum += term;
+        }
+
+        double approxPi = sum * 4; // multiply by 4 to get pi
+
+        // Print results
+        System.out.println("pi according to Java: " + Math.PI);
+        System.out.println("pi, approximated: " + approxPi);
 	}
 }

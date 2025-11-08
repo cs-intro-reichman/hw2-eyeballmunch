38760520aca196a57f28b396760a9403ae484e89
Adi grayson <adi.grayson30@gmail.com>
2025-11-08 19:46:25 +0200

    finally managed to make it print the first line properly

--- Collatz.java
+++ Collatz.java
@@ -6,35 +6,37 @@ public class Collatz {
 		String mode = args[1];
 
 		for (int seed = 1; seed <= N; seed++) {
-			int current = seed;
-			int steps = 1; 
-
-			if (mode.equals("v")) {
-				System.out.print(current);
-			}
-
-			// i really tried with all my power not to use do while 
-			do {
-
-				steps++;
-
-				if (current != 1) { 
-					if (current % 2 == 0) {
-						current /= 2;
-					} else {
-						current = 3 * current + 1;
-					}
-
-					if (mode.equals("v")) {
-						System.out.print(" " + current);
-					}
-				}
-			} while (current != 1);
-
-			if (mode.equals("v")) {
-				System.out.println(" (" + steps + ")");
-			}
-		}
+            int current = seed;
+            int steps = 1;
+            
+            // In verbose mode, print the sequence
+            if (mode.equals("v")) {
+                System.out.print(current + " ");
+            }
+            
+            // Generate the hailstone sequence until we reach 1
+            do {
+                if (current % 2 == 0) {
+                    // If even, divide by 2
+                    current = current / 2;
+                } else {
+                    // If odd, multiply by 3 and add 1
+                    current = (current * 3) + 1;
+                }
+                
+                steps++;
+                
+                // In verbose mode, print each number in the sequence
+                if (mode.equals("v")) {
+                    System.out.print(current + " ");
+                }
+            } while (current != 1);
+            
+            // In verbose mode, print the number of steps
+            if (mode.equals("v")) {
+                System.out.println("(" + steps + ")");
+            }
+        }
 
 		
 		System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
